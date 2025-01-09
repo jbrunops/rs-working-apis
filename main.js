992a46e689744e1ce53e198b589b037e1ddbe2e6
Jackson Porciúncula <jbrunops@outlook.com>
2025-01-09 20:55:18 -0300

    async e await

---
+++ main.js
@@ -0,0 +1,11 @@
+// fetch("http://localhost:3333/products").then((response) => {
+//   console.log(response);
+// });
+
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products");
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchProducts();
